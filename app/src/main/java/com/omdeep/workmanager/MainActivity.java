dda813c0cfa1ce0e0bb0d66f0f367f9d4f2aab05
Omdeep <omdeep1990@gmail.com>
2022-03-29 14:48:53 +0530

    Use of lamda in work manager

---
+++ app/src/main/java/com/omdeep/workmanager/MainActivity.java
@@ -0,0 +1,43 @@
+package com.omdeep.workmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import android.os.Bundle;
+
+import com.omdeep.workmanager.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+Constraints constraints = new Constraints
+        .Builder()
+        .setRequiresCharging(true)
+        .setRequiresBatteryNotLow(false)
+        .build();
+
+        OneTimeWorkRequest request = new OneTimeWorkRequest
+                .Builder(MyWorker.class)
+                .setConstraints(constraints)
+                .build();
+
+        WorkManager.getInstance(this).enqueue(request);
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(request.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                binding.tvWorkStatus.setText(workInfo.getState().name());
+            }
+        });
+    }
+}
